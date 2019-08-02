bc00640f150e7e831f90102e2b339a1bf4c2ff8b
AOF96 <49664328+AOF96@users.noreply.github.com>
2019-08-02 15:40:27 +1200

    Create counts.c

---
+++ LAB3/counts.c
@@ -0,0 +1,19 @@
+/* Question 10 of Lab3. C program that reads its standard input until
+ * EOF occurs and prints out a table of how many times each of the 26
+ * letters in the alphabet occurred.
+ *
+ * Author: Adrian Osuna */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define N_MAX = 26
+
+int main(void)
+{
+    int counts[N_MAX] = {0};
+    int c;
+    int i = 0;
+
+    return EXIT_SUCCESS;
+}
