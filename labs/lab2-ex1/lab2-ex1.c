32a594c8c15afe38318844227fc8fc04b535f0d4
AOF96 <49664328+AOF96@users.noreply.github.com>
2019-10-02 14:10:08 +1300

    Add files via upload

--- labs/lab2-ex1/lab2-ex1.c
+++ labs/lab2-ex1/lab2-ex1.c
@@ -7,22 +7,36 @@ int main (void)
 {
     system_init ();
     led_init ();
-    
+
+    TCCR1A=0x00;
+    TCCR1B=0x05;
+    TCCR1C=0x00;
+    DDRC |= (1 << 2);
     /* TODO: Initialise timer/counter1.  */
-    
+    void timer_delay(uint16_t ticks)
+    {
+        TCNT1 = 0;
+
+        while (TCNT1 < ticks) {
+            continue;
+        }
+    }
+
+
+    uint16_t resolution = 0.5 / 0.000128;
     while (1)
     {
-        
+
         /* Turn LED on.  */
         led_set (LED1, 1);
-        
+
         /* TODO: wait for 500 milliseconds.  */
-        
+        timer_delay(resolution);
         /* Turn LED off.  */
         led_set (LED1, 0);
-        
+
         /* TODO: wait for 500 milliseconds.  */
-        
+        timer_delay(resolution);
     }
-    
+
 }
